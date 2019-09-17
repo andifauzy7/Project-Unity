802961bf57b483e847cb81adf5b9b819431e930c
Irvan Kadhafi <irvan.kadhafi.tif18@polban.ac.id>
2019-09-17 20:37:04 +0700

    Penambahan menu settings, dan mencoba menghitung inventory

---
+++ Assets/Scripts/Senjata.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Senjata : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "pacman")
+        {
+            GameManager.dipakai += 1;
+            GameObject[] senjata = GameObject.FindGameObjectsWithTag("senjata");
+            Destroy(gameObject);
+            if (GameManager.dipakai > 2)
+                GameManager.dipakai = 0; /* Pengen ngebuat kalau isi tas udah sudah, maka muncul pesan   */
+        }
+    }
+}
---
+++ Assets/Settings.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class Settings : MonoBehaviour
+{
+    GameObject panelSetting;
+    public AudioMixer audioMixer;
+    public Slider sliderBGM;
+    public Dropdown resolution;
+    Resolution[] resolutions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelSetting = this.gameObject;
+        resolutions = Screen.resolutions;
+        resolution.ClearOptions();
+
+        List<string> options = new List<string>();
+        int curentResolutionIndex = 0;
+        for(int i=0;i<resolutions.Length;i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width==Screen.currentResolution.width && 
+                resolutions[i].height==Screen.currentResolution.height)
+            {
+                curentResolutionIndex = i;
+            }
+        }
+        resolution.AddOptions(options);
+        resolution.value = curentResolutionIndex;
+        resolution.RefreshShownValue();
+    }
+    
+    public void SetVolumeBGM(float volume)
+    {
+        audioMixer.SetFloat("BGM",volume);
+    }
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
