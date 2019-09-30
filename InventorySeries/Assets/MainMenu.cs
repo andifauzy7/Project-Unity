c176265a7c11feab0161f65db38ead6a296ff890
andifauzy7 <andifauzy00@gmail.com>
2019-09-30 08:10:52 +0700

    Update Inventory

---
+++ InventorySeries/Assets/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Application.LoadLevel("game");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
